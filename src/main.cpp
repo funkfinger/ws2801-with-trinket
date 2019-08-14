bb8046a18fe707c677e26941388549cdb5863b2a
Jay Wiggins <jay@irk.com>
2019-08-13 22:59:53 -0700

    pushbutton with debounce

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,47 @@
 CRGB leds[NUM_LEDS];
 
 unsigned long previousMillis = 0;
-const long interval = 1000;
+const long interval = 200;
+const int debounceTime = 100;
+long buttonDownTimeLimit = 8000;
+long currentDownTime = 0;
+int colorIndex = 0;
+
+CRGB colorArray[] = {CRGB::Green, CRGB::Red, CRGB::Purple, CRGB::BlanchedAlmond, CRGB::CadetBlue, CRGB::DarkGoldenrod};
+
+bool debouncedButton()
+{
+  if (digitalRead(BUTTON_PIN) == HIGH)
+  {
+    currentDownTime = 0;
+    return false;
+  }
+  if (currentDownTime > buttonDownTimeLimit)
+    return true;
+  currentDownTime += 1;
+  return false;
+}
 
 void setup()
 {
   FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-  leds[0] = CRGB::Green;
+  leds[0] = colorArray[colorIndex];
   FastLED.show();
   pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(BUTTON_PIN, HIGH);
 }
 
 void loop()
 {
+  if (debouncedButton())
+  {
+    colorIndex = colorIndex > 5 ? 0 : colorIndex + 1;
+  }
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval)
   {
     previousMillis = currentMillis;
-    leds[0] = leds[0] ? CRGB::Black : CRGB::Green;
+    leds[0] = leds[0] ? CRGB::Black : colorArray[colorIndex];
     FastLED.show();
   }
 }
